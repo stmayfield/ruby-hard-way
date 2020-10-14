955ac2707afdbed0a585d7382cd5a2d2660bd81e
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-13 21:40:57 -0700

    completed excercise 01

---
+++ Excercise 01/ex1.rb
@@ -0,0 +1,18 @@
+puts "Hello World!"
+# Hello World!
+puts "Hello Again"
+# Hello Again
+puts "I like typing this."
+# I like typing this.
+puts "This is fun."
+# This is fun.
+puts "Yay! Printing."
+# Yay! Printing.
+puts "I'd much rather you 'not'."
+# Ruby reads everything between "" as a sting and ignores ''
+puts 'I "said" do not touch this.'
+# Ruby reads everything between '' as a string and ignores ""
+
+
+puts "I \"said\" do not touch this"
+# Ruby also uses an escape to read "" as part of the string inside ""
