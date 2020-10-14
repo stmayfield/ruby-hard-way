38e5852c4d4cf9cbc40a12720db67392d4fceb00
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-14 11:03:56 -0700

    completed study drills

--- ex6.rb
+++ ex6.rb
@@ -1,7 +1,13 @@
+# Define Variables
+
+# set types_of_people var equal to 10
 types_of_people = 10
+# set x var equal to (string w/ variable)
 x = "There are #{types_of_people} types of people."
+# Set binary var and do_not var to (string)
 binary = "binary"
 do_not = "don't"
+# set y var equal to (string w/ variable)
 y = "Those who know #{binary} and those who #{do_not}."
 
 puts x
@@ -33,6 +39,7 @@ w = "This is the left side of..."
 e = "a string with a right side."
 
 puts w + e
+# w + e concatenates to create a longer string 
 
 #Output
 
