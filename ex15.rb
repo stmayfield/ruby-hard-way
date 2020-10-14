ba489d5525904e7e564b1ffb7a5b3b42b87199db
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-14 16:39:00 -0700

    added sample text file, added comments to ex15.rb before first run

---
+++ ex15.rb
@@ -0,0 +1,23 @@
+# Input: ex15.txt
+
+# Receives first argument from command line (ex15.txt) and assigns it to a variable
+filename = ARGV.first
+
+# open method takes in received argument from command line (ex15.txt), then open file method is assigned to txt
+txt = open(filename)
+
+# Outputs interpolated string > Here's your file ex15.txt:
+puts "Here's your file #{filename}:"
+# Outputs the entire contents (obtained from read method) of argument from command line (ex15.txt), provided via the open method
+print txt.read
+
+# Outputs non-interpolated string
+print "Type the filename again: "
+# Command line prompts user for input and assigns input to the variable 'file_again'
+file_again = $stdin.gets.chomp
+
+# open method takes in the received user input from command line prompt and assigns to the variable txt_again 
+txt_again = open(file_again)
+
+# Outputs the entire contents (obtained from read method) of specified user input, served via the open method assigned to txt_again
+print txt_again.read
