b68cfed6fde91b9bbd01e5323ce9fa11438a80ad
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-14 08:30:28 -0700

    fixed comment errors

--- ex2.rb
+++ ex2.rb
@@ -1,8 +1,8 @@
-# A comment, this is so you can read your program later
+# A comment, this is so you can read your program later.
 
 # Anything after the # is ignored by ruby.
 
-puts "I could have code like this." # and the domment after is ignored
+puts "I could have code like this." # and the comment after is ignored
 
 # You can also use a comment to "disable" or comment out a pice of code:
 
