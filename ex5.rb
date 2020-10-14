021833c331851cb3720c38310890db28869adf44
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-14 09:46:22 -0700

    completed ex5

---
+++ ex5.rb
@@ -0,0 +1,32 @@
+# Assigned variables
+my_name = 'Zed A. Shaw'
+my_age = 35 #not a lie in 2009
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+# Output
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+# Expected Output:
+
+# Let's talk about Zed A. Shaw.
+# He's 74 inches tall.
+# He's 180 pounds heavy.
+# Actaully that's not too heavy.
+# He's got Blue eyes and Brown hair.
+# His teeth are usually White depending on the coffee.
+
+# If I add 35, 74, and 180 I get 289.
