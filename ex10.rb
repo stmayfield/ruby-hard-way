5c40ce7f4be3b8468c7d00b5585ccdd06f1eac77
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-14 13:57:47 -0700

    complete ex10.rb

---
+++ ex10.rb
@@ -0,0 +1,20 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+# escape sequences
+# \t > TAB
+# \n > new line (ASCII linefeed)
+# \\ > \ (single backslash)
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnup\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
