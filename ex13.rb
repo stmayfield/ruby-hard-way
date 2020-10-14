4dfa3771583def0ef94b93cd145c131000a9377b
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-14 15:53:24 -0700

    completed ex13.rb

---
+++ ex13.rb
@@ -0,0 +1,16 @@
+# Argument variable
+primary, secondary, tertiary = ARGV
+
+puts "Your first variable is: #{primary}"
+puts "Your second variable is: #{secondary}"
+puts "Your third variable is: #{tertiary}"
+
+# Input
+
+# $ ruby .\ex13.rb 1st 2nd 3rd
+
+# Output
+
+# Your first variable is: 1st
+# Your second variable is: 2nd
+# Your third variable is: 3rd
