be4a28fbff5bd87a12509e2ba81875a076cac2b2
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-14 08:47:06 -0700

    completed excercise 3

---
+++ ex3.rb
@@ -0,0 +1,44 @@
+puts "I will now count my chickens:"
+# string
+
+puts "Hens #{25 + 30 / 6}"
+
+# Guess: Hens 30 | Actual: Hens 30
+
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# Guess: Roosters #{Twenty-five times three, remainder 4, subtracted from one hundred} | Actual: Roosters 97
+
+puts "Now I will count the eggs:"
+# string
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# integer
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+# string
+
+puts 3 + 2 < 5 - 7
+# boolean (false)
+
+puts "What is 3 + 2? #{3 + 2}"
+# Guess: What is 3 + 2? 5 | Actual: matches
+
+puts "What is 5 - 7? #{5 - 7}"
+# Guess: What is 5 - 7? -2 | Actual: matches
+
+puts "Oh, that's why it's false."
+# string
+puts "How about some more."
+# string
+puts "Is it greater? #{5 > -2}"
+# guess: boolean (true) | actual: Is it greater? true
+puts "Is it greater or equal? #{5 >= -2}"
+# guess: boolean (true) | actual: Is it greater or equal? true
+puts "Is it less or equal? #{5 <= -2}"
+# guess: boolean (false) | actual: Is it less or equal? false
+
+
+
+
+
