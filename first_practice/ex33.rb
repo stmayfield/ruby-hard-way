477cf42468436f74ec1fd7c9ce3cdae034cda312
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-21 09:42:00 -0700

    completed excercise 33

---
+++ first_practice/ex33.rb
@@ -0,0 +1,22 @@
+i = 0
+numbers = []
+
+# while loop iterates based on i = 0. 1 is added to i each loop, so loops evaluates to false when i = 6 (0, 1, 2, 3, 4, 5).length = 6
+while i < 6
+    # inter-string "At the top i is 0"
+    puts "At the top i is #{i}"
+    # 0 is sent to the numbers array
+    numbers.push(i)
+
+    # 1 is added to zero and replaces existing variable in i
+    i += 1
+    # "Numbers now: " [0]
+    puts "Numbers now: ", numbers
+    # "At the bottom i is 1"
+    puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
+
+# for loop runs after while loop evaluates to false, iterates through the array
+numbers.each {|num| puts num}
