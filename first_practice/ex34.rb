46487adbfbc71c477d2a0b45e8e8bccf25996644
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-10-21 10:43:24 -0700

    completed excercise 34

---
+++ first_practice/ex34.rb
@@ -0,0 +1,20 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+# The animal at 1. -> ruby
+# The third (3rd) animal. -> kangaroo
+# The first (1st) animal. -> ruby
+# The animal at 3. -> kangaroo
+# The fifth (5th) animal. -> platypus
+# The animal at 2. -> peacock
+# The sixth (6th) animal. -> none
+# The animal at 4. -> whale
+
+puts "The animal at #{animals[1]}."
+puts "The third (3rd) animal."
+puts "The first (1st) animal"
+puts "The animal at #{animals[3]}."
+puts "The fifth (5th) animal."
+puts "The animal at #{animals[2]}."
+puts "The sixth (6th) animal."
+puts "The animal at #{animals[4]}."
