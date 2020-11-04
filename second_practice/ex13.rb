0ac41f1311e9049d2f16e2851e95286f6060aabd
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-11-04 13:55:30 -0800

    completed ex13

---
+++ second_practice/ex13.rb
@@ -0,0 +1,6 @@
+# Multiple variables set to same hold (ARGV)
+first, second, third = ARGV
+
+puts "Your first var is: #{first}"
+puts "Your second var is: #{second}"
+puts "Your third var is: #{third}"
