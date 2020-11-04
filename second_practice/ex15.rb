5f1c64336cce21d5d1177405e0898ba4ef578290
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-11-04 14:29:19 -0800

    refactored ex15 with if/else method

--- second_practice/ex15.rb
+++ second_practice/ex15.rb
@@ -1,13 +1,32 @@
-filename = ARGV.first
+# filename = ARGV.first
 
-text = open(filename)
+# text = open(filename)
 
-puts "Here's your file #{filename}:"
-print text.read
+# puts "Here's your file #{filename}:"
+# print text.read
 
-print "Type the filename again: "
-file_again = $stdin.gets.chomp
+# print "Type the filename again: "
+# file_again = $stdin.gets.chomp
 
-text_again = open(file_again)
+# text_again = open(file_again)
 
-print text_again.read
+# print text_again.read
+
+# ex15.txt
+
+# ---
+
+def open_file
+    print "Enter the name of your file: "
+    file_name = $stdin.gets.chomp
+        if File.exists?(file_name)
+            file = open(file_name)
+            print file.read
+            file.close
+        else
+            puts "No file found!"
+            open_file()
+        end
+end
+
+open_file()
