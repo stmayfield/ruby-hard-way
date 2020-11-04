4146101b07a34db57ff17607d05c5328936d6324
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-11-04 10:41:53 -0800

    completed ex05.rb

---
+++ second_practice/ex05.rb
@@ -0,0 +1,18 @@
+# variables
+my_name = 'Stephen Mayfield'
+my_age = 27
+my_height = (5 * 12) + 8
+my_weight = 150
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Light Brown'
+
+# interpolated strings
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He weighs #{my_weight} pounds."
+puts "Actaully, that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth}, depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_height + my_weight}."
